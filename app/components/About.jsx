bb7f6b28431cd811e0d9c61dd654f29beecacd0c
jackwmarkham <markham.jack.w@gmail.com>
2016-11-24 10:56:04 +1100

    Styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // var About = React.createClass({
 //   render: function () {
 //     return (
@@ -13,8 +13,12 @@ var React = require('react');
 var About =  (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a simple react app for demonstrating API calls and basic routing in React.<br/> </p>
+      <p>
+        Made by <a href="http://jackmarkham.herokuapp.com/">Jack Markham.</a><br/>
+        Source code available on <a href="https://github.com/jackwmarkham/ReactWeather">GitHub</a>
+    </p>
     </div>
   )
 };
